c6577a0e6d48b85585e22185669c03207e00f38a
xiao fu <tianxuefeng2010@gmail.com>
2023-08-24 16:56:25 +0800

    hh

--- Example/Pods/Target Support Files/Pods-tenFaceLiveSDK_Example/Pods-tenFaceLiveSDK_Example-resources.sh
+++ Example/Pods/Target Support Files/Pods-tenFaceLiveSDK_Example/Pods-tenFaceLiveSDK_Example-resources.sh
@@ -98,11 +98,9 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/NTESLiveDetect/Assets/NTESLiveDetectBundle.bundle"
-  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/tenFaceLiveSDK/tenFaceLiveSDK.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/NTESLiveDetect/Assets/NTESLiveDetectBundle.bundle"
-  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/tenFaceLiveSDK/tenFaceLiveSDK.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
